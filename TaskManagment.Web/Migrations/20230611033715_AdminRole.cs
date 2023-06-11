e6c683413911f28eab7b44ce486ae736ebdbf505
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2023-06-10 23:59:09 -0400

    Roles
    Create Role in Migration

---
+++ TaskManagment.Web/Migrations/20230611033715_AdminRole.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagment.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"if not exists(select id from AspNetRoles where Id = 'ebc70731-447f-4252-8953-56f6eb9ba85f')
+BEGIN
+Insert AspNetRoles (Id, [Name], [NormalizedName])
+Values ('ebc70731-447f-4252-8953-56f6eb9ba85f', 'admin', 'ADMIN')
+END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles where Id = 'ebc70731-447f-4252-8953-56f6eb9ba85f'");
+        }
+    }
+}
